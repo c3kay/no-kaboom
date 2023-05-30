4e8161caa4db6678e0cf8f2052d6bec7220b86f3
Calvin <c3kay@icloud.com>
2023-05-30 20:23:26 +0200

    init v0.0.1

---
+++ src/main/java/de/c3kay/NoKaboom/NoKaboom.java
@@ -0,0 +1,49 @@
+package de.c3kay.NoKaboom;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
+
+public class NoKaboom extends JavaPlugin implements Listener {
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin disabled");
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+
+        // copy embedded config if needed
+        saveDefaultConfig();
+
+        getLogger().info("NoKaboom " + getDescription().getVersion() + " enabled");
+    }
+
+    @EventHandler
+    public void onExplode(EntityExplodeEvent e) {
+        final Map<EntityType, String> explosiveEntities = Map.of(
+                EntityType.PRIMED_TNT, "tnt",
+                EntityType.MINECART_TNT, "tnt",
+                EntityType.ENDER_CRYSTAL, "endercrystal",
+                EntityType.CREEPER, "creeper",
+                EntityType.FIREBALL, "ghast"
+        );
+
+        final EntityType explodedEntity = e.getEntityType();
+
+
+        if (
+                explosiveEntities.containsKey(explodedEntity)
+                && getConfig().getBoolean(explosiveEntities.get(explodedEntity), false)
+        ) {
+                e.setCancelled(true);
+                getLogger().info("Canceled explosion of " + explodedEntity);
+        }
+    }
+}
